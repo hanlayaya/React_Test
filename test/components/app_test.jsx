3fabef2c917c0a814c4bfdae8c593e6a14c2c516
David_Yu <yuyang545262477@gmail.com>
2016-06-20 23:13:25 +0800

    finish one child test

--- test/components/app_test.jsx
+++ test/components/app_test.jsx
@@ -3,12 +3,12 @@ import App from "../../src/components/app";
 
 describe('App', () => {
     let component;
-    
+
     beforeEach(() => {
         component = renderComponent(App);
     });
-    
+
     it('renders something', () => {
-        expect(component).to.exist;
+        expect(component.find('.comment-box')).to.exist;
     });
 });
